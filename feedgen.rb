9f8503cef2a280d49fa19bd47c48745838d6dd4e
Attila Malarik <attila.malarik@gmail.com>
2017-09-10 11:04:06 +0200

    first import of all server side goodness

---
+++ feedgen.rb
@@ -0,0 +1,40 @@
+#!/usr/bin/env ruby
+
+require 'yaml'
+require 'json'
+require 'time'
+
+logDir = 'log'
+profileFile = 'profile.yml'
+
+outputFile = 'feed.json'
+
+data = []
+Dir.glob "#{logDir}/*.yml" do |fname|
+  yam = YAML.load_file(fname)
+  data.concat yam['data']
+end
+
+data = data.sort { |a, b| b['time'] <=> a['time'] }.take(30)
+
+profile = YAML.load( File.open(profileFile, 'r') { |f| f.read } )
+json = {}
+json['profile'] = profile['profile']
+json['portal'] = profile['portal']
+json['feed'] = []
+
+data.each { |entry|
+  res = {}
+  entry.each { |key, value|
+    res[key] = case key
+      when "time"
+        Time.parse(value).tv_sec
+      else
+        value
+      end
+  }
+
+  json['feed'].push res
+}
+
+File.open(outputFile, 'w') { |f| f.write( JSON.dump(json) ) }
---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: PUT, GET, POST, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: *");
+
+$f = 'feed.json'; // path to rotonde feed json file
+
+if ( !file_exists($f) || (filectime($f) < (time()-60*60)) ) {
+	echo `ruby feedgen.rb`;
+}
+
+$feed = json_decode(file_get_contents($f), true);	
+
+echo json_encode($feed, JSON_UNESCAPED_SLASHES);
+?>
